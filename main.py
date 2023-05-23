3ef4481cb0e65830ce014f60c9598c40bca5fcaf
vladik1998 <shembel.vladik@gmail.com>
2023-05-24 02:02:27 +0300

    added generator

--- main.py
+++ main.py
@@ -9,10 +9,63 @@ import sys
 
 # print(f"Action: {action}, In Path: {inPath}, Out Path: {outPath}")
 
-with open("input.txt") as intxt:
-    text = intxt.readlines()
-    print(text)
-# detect sequences
-def detection(text):
-    for line in text:
-        words = str(line).split(" ")
+
+def main():
+    makeDictionaryOfWords()
+
+
+def readFile():
+    # read the input
+    with open("input.txt") as intxt:
+        text = intxt.read()
+        return text
+
+
+def makeDictionaryOfWords():
+    text = readFile()
+    # first we split by spaces
+    wordsList = text.split()
+    print(wordsList)
+    dic = {}
+    for word in wordsList:
+        if len(word) > 1:
+            if word in dic:
+                dic[word] += 1
+            else:
+                dic[word] = 1
+    print(dic)
+    gen = CharsGen()
+    # TODO: deal with reverse words..
+
+
+class CharsGen:
+    def __init__(self):
+        self.nextChar = None
+        charList = []
+        for i in range(33, 127):
+            ch = chr(i)
+            if not ch.isalpha() and not ch.isdigit():
+                charList.append(ch)
+        self.charsList = charList
+        self.current_first = charList[0]
+        self.current_second = charList[0]
+
+    def getNextChar(self):
+        symbol = f"{self.current_first}{self.current_second}"
+
+        last_index = len(self.charsList) - 1
+        second_index = self.charsList.index(self.current_second)
+        first_index = self.charsList.index(self.current_first)
+
+        if second_index != last_index:
+            self.current_second = self.charsList[second_index + 1]
+        elif first_index != last_index:
+            self.current_first = self.charsList[first_index + 1]
+            self.current_second = self.charsList[0]
+        else:
+            raise IndexError("No more characters can be generated.")
+        return symbol
+
+
+if __name__ == "__main__":
+    main()
